3af0928e0c46c344fda11f34f1e734f3fb18d65f
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-05-05 13:20:40 +0300

    Arrays exercises solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,33 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
-      end
+        result = []
+        max = array.max()
+        array.each { |number|
+            if number > 0
+                result.push(max)
+            else
+                result.push(number)
+            end
+        }
+        return result
+    end
 
       def search(_array, _query)
-        0
+        low = 0
+        high = _array.length() - 1
+        while low <= high
+            index = (low + high) / 2
+            number = _array[index]
+            if number == _query
+                return index
+            elsif number > _query
+                high = index - 1
+            else
+                low = index + 1
+            end
+        end
+        return -1
       end
     end
   end
