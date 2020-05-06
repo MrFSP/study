27e6ba356f4ddbea1f74877b72398abf545deb03
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-05-06 12:20:10 +0300

    fp exercises solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,18 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        films = _array
+          .filter { |film| film["rating_kinopoisk"] && film["rating_kinopoisk"].to_f > 0 }
+          .filter { |film| film["country"] && film["country"].split(',').count() >= 2 }
+        rating = films
+          .reduce(0) { |sum, film| sum + film["rating_kinopoisk"].to_f }
+        return rating / films.count
       end
 
       def chars_count(_films, _threshold)
-        0
+        _films
+          .filter { |film| film["rating_kinopoisk"] && film["rating_kinopoisk"].to_f > _threshold }
+          .reduce(0) { |count, film| count + film["name"].count("и") }
       end
     end
   end
