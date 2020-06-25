630a4ab4cda91f3fdb0e21be83a1d77519aca537
Pavel Egorov <PavelEgorov@MacBookPro-MrFSP.local>
2020-06-25 15:55:46 +0300

    solution

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,33 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      @routes ||= {}
+      instance_eval(&block)
     end
 
     def call(env)
+      req = Rack::Request.new(env)
+      method = req.request_method.downcase.to_sym
+      path = req.path_info
+      return missing if @routes[method][path].nil?
+      @routes[method][path].call
+    end
+
+    def add_route(method, path, &block)
+      @routes[method] ||= {}
+      @routes[method][path] = block
+    end
+
+    def method_missing(method, *args, &block)
+      if [:get, :post].include?(method)
+        add_route(method, *args, &block)
+      else
+        super
+      end
+    end
+
+    def missing
+      [404, {}, ['Not Found']]
     end
   end
 end
