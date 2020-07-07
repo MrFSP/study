8408834ae32630d80def7b2a29a4fe19054453c3
Pavel Egorov <epe.m@mail.ru>
2020-07-07 05:43:51 +0300

    solution refactored

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,20 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array.map { |el| el > 0 ? 100 : el }
+        max = array.reduce { |acc, number| number > acc ? number : acc }
+        array.map { |el| el > 0 ? max : el }
       end
 
       def search(array, query)
         iter(0, array.length - 1, array, query)
       end
 
-      def iter(min_index, max_index, array, query)
-        mid_index = (min_index + max_index) / 2
+      def iter(left, right, array, query)
+        mid_index = (left + right) / 2
         number = array[mid_index]
         return mid_index if number == query
-        return -1 if min_index >= max_index
-        return iter(min_index, mid_index, array, query) if number > query
-        iter(mid_index + 1, max_index, array, query)
+        return -1 if left >= right
+        return iter(left, mid_index, array, query) if number > query
+        iter(mid_index + 1, right, array, query)
       end
     end
   end
